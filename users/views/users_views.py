7a98f3f8d04293f658010233a350cfec2891a2ac
Mubassir Habib <66573095+Mubassir1820@users.noreply.github.com>
2026-01-14 00:57:32 +0600

    Implementation of user profile API

--- users/views/users_views.py
+++ users/views/users_views.py
@@ -1,7 +1,22 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from users.serializers.users_serializer import CustomTokenObtainPairSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 class LoginView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
     permission_classes = [AllowAny]
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "id": user.id,
+            "email": user.email,
+            "role": user.role,
+            # "gym_branch": user.gym_branch.id if user.gym_branch else None
+        })
